44fe07ce374ba8e7b68faa3f35087310aec1b082
Harryi0 <yucheng.ji.harry@outlook.com>
2021-03-22 23:58:35 +0000

    update data content

--- SocialEvolutionDataset.py
+++ SocialEvolutionDataset.py
@@ -2,6 +2,7 @@ import os.path as osp
 
 import torch
 import pandas
+import pandas as pd
 from torch_geometric.data import InMemoryDataset, TemporalData, download_url
 
 
@@ -33,16 +34,21 @@ class SocialEvolutionDataset(InMemoryDataset):
     #     download_url(self.url.format(self.name), self.raw_dir)
 
     def process(self):
-        df = pandas.read_csv(self.raw_paths[0], skiprows=1, header=None)
-
+        # df = pandas.read_csv(self.raw_paths[0], skiprows=1, header=None)
+        df = pandas.read_csv(self.raw_paths[0])
+        df.dt = pd.to_datetime(df.dt)
+        df['dt_hr'] = df.dt - df.dt[0]
+        def timedelta2hour(x):
+            return round((x.days * 24 + x.seconds / 3600), 3)
+        df['dt_hr'] = df['dt_hr'].apply(timedelta2hour)
         src = torch.from_numpy(df.iloc[:, 0].values).to(torch.long)
         dst = torch.from_numpy(df.iloc[:, 1].values).to(torch.long)
-        # dst += int(src.max()) + 1
         t = torch.from_numpy(df.iloc[:, 2].values).to(torch.long)
         y = torch.from_numpy(df.iloc[:, 3].values).to(torch.long)
+        dt_hr = torch.from_numpy(df.iloc[:, 5].values).to(torch.float)
         # msg = torch.from_numpy(df.iloc[:, 4:].values).to(torch.float)
 
-        data = TemporalData(src=src, dst=dst, t=t, y=y)
+        data = TemporalData(src=src, dst=dst, t=t, y=y, dt_hr=dt_hr)
 
         if self.pre_transform is not None:
             data = self.pre_transform(data)
